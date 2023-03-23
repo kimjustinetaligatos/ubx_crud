ef0abed3cc2ec67eff70ec3087eebb834871b4e0
kimjusteeen <kjtaligatos13@gmail.com>
2023-03-23 13:45:49 +0800

    Frontend - Car Brand Models Crud

--- app/app.js
+++ app/app.js
@@ -61,6 +61,7 @@ angular.module('clientApp').controller("CommonController", function ($scope, Htt
         $scope.UpdateCarBrandParam.brand_name = car.brand_name;
     }
 
+    //UPDATE
     $scope.UpdateCarBrand = function (){
         HttpRequest.Request($scope.API_URL, $scope.UpdateCarBrandParam, "PUT").success(function (data) {
             if(data.result){
@@ -68,6 +69,67 @@ angular.module('clientApp').controller("CommonController", function ($scope, Htt
                 $scope.GetCarBrands();
                 $scope.ActiveEdit = undefined;
                 $scope.UpdateCarBrandParam = {};
+            }
+        });
+    }
+
+    $scope.SetViewCarBrandModels = function (car){
+        $scope.ViewCarBrandModels = car;
+        $scope.GetCarBrandModels(car.id);
+    }
+
+    //GET
+    $scope.GetCarBrandModels = function(brand_id){
+        HttpRequest.Request($scope.API_URL + "brand/" + brand_id, [], "GET").success(function (data) {
+            $scope.CarBrandModels = data;
+        });
+    }
+
+    //DELETE
+    $scope.DeleteCarBrandModel = function (brand_id){
+        HttpRequest.Request($scope.API_URL + "brand/" + brand_id, [], "DELETE").success(function (data) {
+            if(data.result){
+                alert("Brand Model Deleted!");
+                $scope.GetCarBrandModels($scope.ViewCarBrandModels.id)
+            }
+        });
+    }
+
+    //SET
+    $scope.SetCarBrandModelParam = {};
+    $scope.SetCarBrandModel = function (){
+        $scope.SetCarBrandModelParam.brand_id = $scope.ViewCarBrandModels.id;
+        HttpRequest.Request($scope.API_URL + "brand/", $scope.SetCarBrandModelParam, "POST").success(function (data) {
+            if(data.result){
+                alert("Brand Model Added!");
+                $scope.GetCarBrandModels($scope.ViewCarBrandModels.id)
+                $scope.SetCarBrandModelParam = {};
+            }
+        });
+    }
+
+    //SET EDIT FIELD
+    $scope.UpdateCarBrandModelParam = {};
+    $scope.SetActiveBrandModelEdit = function (car) {
+        if (!car) {
+            //RESET
+            $scope.ActiveBrandModelEdit = undefined;
+            $scope.UpdateCarBrandModelParam = {};
+            return;
+        }
+        $scope.ActiveBrandModelEdit = car.id;
+        $scope.UpdateCarBrandModelParam.id = car.id;
+        $scope.UpdateCarBrandModelParam.model_name = car.model_name;
+    }
+
+    //UPDATE
+    $scope.UpdateCarBrandModel = function (){
+        HttpRequest.Request($scope.API_URL + "brand/", $scope.UpdateCarBrandModelParam, "PUT").success(function (data) {
+            if(data.result){
+                alert("Brand Model Updated!");
+                $scope.GetCarBrandModels($scope.ViewCarBrandModels.id)
+                $scope.ActiveBrandModelEdit = undefined;
+                $scope.UpdateCarBrandModelParam = {};
             }
         });
     }
